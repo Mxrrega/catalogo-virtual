d966885c03279957eea6e1819b17a75cebb73c97
Mxrrega <matheusmarregamarrega@gmail.com>
2023-09-13 16:43:16 -0300

    Adição do cadastro e login MUI

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import Cadastro from './Cadastro';
 import { GlobalStyles } from '@mui/material';
 import Filmes from './Filmes';
 import EditaFilme from './EditaFilme';
+import Album from './Album';
+import LoginMUI from './LoginMUIi';
+import CadastroMUI from './CadastroMUI';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +32,18 @@ const router = createBrowserRouter([
   {
     path: "/edicao/:id",
     element: <EditaFilme />
+  },
+  {
+    path: "/Album",
+    element: <Album />
+  },
+  {
+    path: "/loginmui",
+    element: <LoginMUI />
+  },
+  {
+    path: "/cadastromui",
+    element: <CadastroMUI />
   }
 ]);
 
